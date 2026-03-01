94e1350f5dee8153815e7fa36e971b5027e0624b
Joheen-2005 <jayjayjaykundu@gmail.com>
2026-03-01 17:17:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
+# config.py
 import streamlit as st
-if "GROQ_API_KEY" not in st.secrets:
-    st.error("GROQ_API_KEY not configured in Streamlit secrets.")
-    st.stop()
+
 GROQ_API_KEY = st.secrets["GROQ_API_KEY"]
